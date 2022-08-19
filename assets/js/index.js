334ec3ef232b277131ab75383b3b83c585b478f6
Drinergy <78583809+Drinergy@users.noreply.github.com>
2022-08-19 16:16:01 +0800

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+// change text and gif when yes button has been clicked
+function change(){
+    document.getElementById('gif')
+    .src="assets/images/yes.gif";
+    document.getElementById('message')
+    .innerHTML="LESGOOO!!!! MWAAAAA!!";
+
+    const no = document.getElementById('naur');
+    no.style.display = 'none';
+
+    const yes = document.getElementById('g');
+    yes.style.display = 'none';
+}
+
+// move the no button randomly
+$('#naur').on('mouseover', function() {
+    $(this).animate({
+      'top': Math.random() * 320 + "px",
+      'left': Math.random() * 320 + "px"
+    }, "medium");
+  })
